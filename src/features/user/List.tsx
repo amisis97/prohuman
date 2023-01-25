e65ea6c7d124e279e43bbdc07cdcb26e3adeabfd
tmisi <misi@ps.hu>
2023-01-25 10:23:57 +0100

    refactor: fetchUser hook

--- src/features/user/List.tsx
+++ src/features/user/List.tsx
@@ -1,23 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Pagination } from '../../ui/pagination';
 import { Column, DataTable } from '../../ui/table';
-import { useFetchUsers } from './api';
+import { useQueryUsers } from './api';
 
 const columns: Column[] = [
   { field: 'id', headerName: 'ID' },
   { field: 'name', headerName: 'Név', flex: 2 },
 ];
 
+const PAGE_SIZE = 10;
+
 export const List = () => {
-  const { users, isLoading } = useFetchUsers({ page: 1 });
+  const [page, setPage] = useState(1);
+  const {
+    users,
+    info: { recordCount },
+  } = useQueryUsers({ page, pageSize: PAGE_SIZE });
 
   const rows = users.map((user) => ({
     id: user.login.uuid,
     name: `${user.name.title} ${user.name.first} ${user.name.last}`,
   }));
 
-  if (isLoading) {
-    return <p>Töltés...</p>;
+  if (users.length === 0) {
+    return <p>Nincs találat!</p>;
   }
 
-  return <DataTable columns={columns} rows={rows}></DataTable>;
+  return (
+    <div>
+      <DataTable columns={columns} rows={rows} />
+      <Pagination page={page} allPage={Math.ceil(recordCount / PAGE_SIZE)} onChange={setPage} />
+    </div>
+  );
 };
--- src/features/user/api.ts
+++ src/features/user/api.ts
@@ -1,37 +1,70 @@
-import { useEffect, useMemo, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import { hasPrime } from '../../utils';
 import { API_URL } from './consts';
 import { Response, User } from './model';
 
 const SEED_ID = 'prohuman';
+const RESULTS_SIZE = 200;
 
-const fetchUsers = async (page: number, results: number) => {
-  const query = new URLSearchParams({
-    seed: SEED_ID,
-    results: String(results),
-  }).toString();
+const query = new URLSearchParams({
+  seed: SEED_ID,
+  results: String(RESULTS_SIZE),
+}).toString();
 
-  const result = await fetch(`${API_URL}?${query}`);
-  const { results: users } = (await result.json()) as Response;
+const useFetchUsers = () => {
+  const [response, setResponse] = useState<Response>({ results: [] });
 
-  return users.filter((user) => hasPrime(user.location.postcode)).slice((page - 1) * 10, page * 10);
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const result = await fetch(`${API_URL}?${query}`);
+      setResponse((await result.json()) as Response);
+    };
+
+    fetchUsers();
+  }, []);
+
+  const { results: users } = response;
+
+  const filteredUsers = useMemo(
+    () => users.filter((user) => hasPrime(user.location.postcode)),
+    [users],
+  );
+
+  return useCallback(
+    async (page: number, pageSize: number) => {
+      return {
+        users: filteredUsers.slice((page - 1) * pageSize, page * pageSize),
+        totalCount: filteredUsers.length,
+      };
+    },
+    [filteredUsers],
+  );
 };
 
 interface UseFetchUsersProps {
   page: number;
+  pageSize?: number;
 }
 
-export const useFetchUsers = ({ page }: UseFetchUsersProps) => {
+export const useQueryUsers = ({ page, pageSize = 10 }: UseFetchUsersProps) => {
+  const fetchUsers = useFetchUsers();
+  const [recordCount, setRecordCount] = useState(0);
   const [users, setUsers] = useState<User[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   useEffect(() => {
-    setIsLoading(true);
-    fetchUsers(page, 200).then((users) => {
+    fetchUsers(page, pageSize).then(({ users, totalCount }) => {
+      setRecordCount(totalCount);
       setUsers(users);
-      setIsLoading(false);
     });
-  }, [page]);
+  }, [page, fetchUsers]);
 
-  return useMemo(() => ({ users, isLoading }), [users, isLoading]);
+  return useMemo(
+    () => ({
+      users,
+      info: {
+        recordCount,
+      },
+    }),
+    [users],
+  );
 };
